98b2f907e462dbda5805b16a6a368f601f2866e2
Jose Carlos <mudeliesgranc@gmail.com>
2026-09-16 17:51:30 +0200

    Inicialización del curso

---
+++ React-Optativa/jsbasico.js
@@ -0,0 +1,76 @@
+/* 
+
+2 + 2
+4
+a = 2
+2
+a + 8
+10
+2 + "2"
+'22'
+2 + hola = error
+hola = "22"
+'22'
+2+ hola
+'222'
+b = 0
+0
+b = "hola"
+'hola'
+"hola".toUpperCase()
+'HOLA'
+"hola".at(2)
+'l'
+"hola".at(4)
+undefined
+"hola".at(-1)
+'a'
+hola = false
+false (es un booleano)
+hola = null
+null
+
+let hola = 33
+undefinded 
+
+const hola2 = 22
+undefined
+
+hola = "dd"
+'dd'
+
+buscar diferencia entre asignacion y declaracion en javascript
+
+a = {} objeto
+
+{let kk22 = 11} bloque de codigo con variable local
+
+true && true
+true
+
+true && false
+false
+
+true  && 11
+11
+
+true && "false"
+'false'
+
+true || true
+true
+
+true || false
+true
+
+buscar que son los cortocircuitos en javascript
+
+|| -> or
+
+&& -> and
+
+buscar truthy y falsy en javascript
+
+
+
+*/
