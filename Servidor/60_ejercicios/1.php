f4afe7cf15540ba3f43171d255b0e87789ac6143
José Carlos <mudeliesgranc@gmail.com>
2026-09-24 14:36:44 +0200

    se borra 60 ejercicios

--- Servidor/60_ejercicios/1.php
+++
@@ -1,8 +0,0 @@
-<?php
-$Sun = 150000000;
-$Pluto = 5,9064x(10**9);
-$Diametro = 1392000;
-echo "Distancia del sol{$Sun}";
-echo "Distancia del sol a plutón {$Pluto}";
-echo "Diametro {$Diametro}";
-?>
